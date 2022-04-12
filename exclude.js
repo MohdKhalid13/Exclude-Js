277841e8c546462a770deaf5cfcb72424554015f
MohdKhalid13 <mohdkhalid.mansoori@gmail.com>
2022-04-12 12:34:53 +0530

    Exclude js

---
+++ exclude.js
@@ -0,0 +1,71 @@
+// type 1  Prompt Method for this you need a html file and this your js file should be link with that
+
+// let a = prompt("enter a number")
+// let b = prompt("enter a number")
+// let c = prompt("enter a number")
+
+// for(let i=a ; i<=b ; i++)
+// {
+//     console.log("<br>")
+
+//    if( i != c )
+//    {
+
+//    console.log(i);
+
+// }
+// }
+
+
+
+
+
+// type 2  filter 
+
+// var ex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// console.log(ex.filter(function(value){
+//     return value !== 6 && value !== 9;
+// }));
+
+
+
+
+
+
+// type 3  Ternary Operator
+
+// for(let i = 1; i<=10; i ++) {
+//     i !=6 && i != 9 ? console.log(i): null
+//   }
+
+
+
+
+
+// type 4  Normal for loop and if statement
+
+//   for(let i = 1; i<=20; i ++) {
+//   if( i !=6 && i != 9){
+//     console.log(i);
+// }
+//   }
+
+
+
+
+// type 5  Functions
+
+
+function hello(x,y,z)
+{
+
+    for(let i=x ; i<=y ; i++){
+        
+        if( i !=  z )
+        {   
+            
+            console.log(i)
+        }
+    }
+}
+hello(1,10,10)
